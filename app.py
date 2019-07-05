e2b2d746ece257a94333488d7cd693805d27bef4
nattawut123 <51470979+nattawut123@users.noreply.github.com>
2019-07-05 11:30:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,11 +24,11 @@ def callback():
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
     #if (userText  == 'สวัสดี') :
-    #    sendText(user, 'สวัสดีกระเจี้ยว56')
+    #      sendText(user, 'สวัสดีกระเจี้ยว56')
     #elif (sendText(user, == 'ไอบ้า') :
-    #     sendText(user,'มึงสิบ้า')
+    #      sendText(user,'มึงสิบ้า')
     #elif (#sendText(user, == 'แต่ก็รักนะจุ้บ') :
-    #    sendText(user,'ขอบคุณครับที่รับชม')
+    #      sendText(user,'ขอบคุณครับที่รับชม')
     return '',200
 
 def sendText(user, text):
